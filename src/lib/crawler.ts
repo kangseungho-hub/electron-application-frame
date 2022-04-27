a598ceff57ee3a78bf4b51f1159c58007583db3f
DESKTOP-0R219S8\seungho <kmc54320@gmail.com>
2022-04-27 13:40:47 +0900

    FIX : fix bug (테스트용 쿠팡 크롤러 한정)

--- src/lib/crawler.ts
+++ src/lib/crawler.ts
@@ -1,5 +1,6 @@
 import {Agent } from "./agent"
 import {parse } from "node-html-parser"
+
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export type searchOption ={
@@ -26,22 +27,38 @@ export class Crawler {
             let parser = parse(html)
 
             let productDivs = parser.querySelectorAll(".search-product")
+
+            //crawling종료 조건1
+            //현재 page에 상품이 없다면
             if(productDivs.length == 0){
                 break;
             }
 
             productDivs.forEach(productDiv => {
-                result.push({
-                    name: productDiv.querySelector(".name").innerText,
-                    price: productDiv.querySelector(".price-value").innerText
-                })
+                let nameDiv = productDiv.querySelector(".name")
+                let priceDiv = productDiv.querySelector(".price-value")
+
+                //product스킵
+                //price value혹은 name가 없는 경우 제외(확인된 상품 : 중고, )
+                if (nameDiv && priceDiv) {
+                    result.push({
+                        name: nameDiv.innerText,
+                        price: priceDiv.innerText
+                    });
+                }
             })    
 
             
             let nextPage = parser.querySelector(".search-pagination .selected+a")
+
+            //crawling종료 조건 2.
+            //다음 page가 없는 경우
             if(nextPage){
                 page += 1
-                await delay(option.delay)
+
+                if(option.delay != 0){
+                    await delay(option.delay)
+                }
             }else{
                 break;
             }
